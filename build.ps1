74a896f9d18d5e62150fa13f7d08e4a66c0f9fcc
Bastian Eicher <bastian.eicher@axoom.com>
2017-09-20 12:38:53 +0200

    Simplified build process (no docker-compose)

--- build.ps1
+++ build.ps1
@@ -1,9 +1,15 @@
 Param ([string]$Version = "0.1-dev")
-$ErrorActionPreference = "Stop"
 Push-Location $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-$env:DOCKER_REGISTRY = if ($Version.Contains("-")) {"docker-ci.axoom.cloud"} else {"docker.axoom.cloud"}
-$env:VERSION = $Version
-docker-compose up --build
+$DockerRegistry = if ($Version.Contains("-")) {"docker-ci.axoom.cloud"} else {"docker.axoom.cloud"}
+
+echo "Building plugin image"
+$ImageId = docker build -q .
+
+echo "Removing old plugin version (if exists)"
+docker plugin rm "$DockerRegistry/docker-volume-simple:$Version"
+
+echo "Building & installing new plugin version"
+docker run --rm --volume /var/run/docker.sock:/var/run/docker.sock $ImageId docker plugin create "$DockerRegistry/docker-volume-simple:$Version" /plugin
 
 Pop-Location
--- build.sh
+++ build.sh
@@ -9,4 +9,11 @@ else
   export DOCKER_REGISTRY="docker.axoom.cloud"
 fi
 
-docker-compose up --build
+echo "Building plugin image"
+ImageId=$(docker build -q .)
+
+echo "Removing old plugin version (if exists)"
+docker plugin rm "$DockerRegistry/docker-volume-simple:$Version"
+
+echo "Building & installing new plugin version"
+docker run --rm --volume /var/run/docker.sock:/var/run/docker.sock $ImageId docker plugin create "$DockerRegistry/docker-volume-simple:$Version" /plugin
