56139e73a8b62ba21d0c63c01da75200b95d2fb9
Bastian Eicher <bastian.eicher@axoom.com>
2017-10-04 16:03:00 +0200

    Tweaked build script

--- build.ps1
+++ build.ps1
@@ -3,13 +3,16 @@ Push-Location $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 $DockerRegistry = if ($Version.Contains("-")) {"docker-ci.axoom.cloud"} else {"docker.axoom.cloud"}
 
-echo "Building plugin image"
+echo "Build Temporary Image"
 $ImageId = docker build -q .
 
-echo "Removing old plugin version (if exists)"
+echo "Removing Old Plugin (if exists)"
 docker plugin rm "$DockerRegistry/docker-volume-simple:$Version"
 
-echo "Building & installing new plugin version"
+echo "Build Plugin"
 docker run --rm --volume /var/run/docker.sock:/var/run/docker.sock $ImageId docker plugin create "$DockerRegistry/docker-volume-simple:$Version" /plugin
+
+echo "Remove Temporary Image"
+docker image rm $ImageId
 
 Pop-Location
